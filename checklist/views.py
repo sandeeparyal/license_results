6da7d2152a9413c2e77e2a4a096b4a8c132837a4
Sandeep Aryal <sandeeparyal@gmail.com>
2023-09-26 11:15:14 +0545

    Ajax implemented

--- checklist/views.py
+++ checklist/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import FileResponse
+from django.core import serializers
+from django.http import JsonResponse
 from django.contrib import messages
 
 from django.contrib.auth.decorators import login_required
@@ -19,8 +20,16 @@ def index(request):
             examination_type = form.cleaned_data['examination_type']
             examination_license_type = form.cleaned_data['examination_license_type']
             checklists = Checklist.objects.filter(examination__examination_date=str(examination_date), examination__examination_type=examination_type, examination__examination_license_type=examination_license_type)
-            messages.success(request, "File found")
-            return render(request, 'checklist/preview.html', {"checklists": checklists })
+            if len(checklists) == 0:
+                errors = "No checklists available for the selection"
+                return JsonResponse({'success':False, 'errors': errors})
+            else:
+                messages.success(request, "File found")
+                return JsonResponse(serializers.serialize('json', checklists), safe=False)
+        else:
+            return JsonResponse({'success': False, 'errors': form.errors})
+
+            #return render(request, 'checklist/preview.html', {"checklists": checklists })
     else: 
         form = ExaminationForm()
         return render(request, 'checklist/index.html', {"form":form }) 
