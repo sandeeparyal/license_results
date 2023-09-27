ca92c12498be3a0fe2cb11dd2f934072a1d243ec
Sandeep Aryal <sandeeparyal@gmail.com>
2023-09-27 13:38:12 +0545

    Interface change, model LicenseType with Null value on Written, Drop down selection

--- checklist/models.py
+++ checklist/models.py
@@ -79,7 +79,7 @@ class LicenseType(models.Model):
          (WRITTEN_CATEGORY, "Not Applicable"),
      }
     
-    license_name = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
+    license_name = models.CharField(max_length=20, choices=CATEGORY_CHOICES, null=True)
     license_description = models.CharField(max_length=100, null=True)
 
     def __str__(self):
--- checklist/views.py
+++ checklist/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
 from django.core import serializers
 from django.http import JsonResponse
 from django.contrib import messages
@@ -20,16 +21,21 @@ def index(request):
             examination_type = form.cleaned_data['examination_type']
             examination_license_type = form.cleaned_data['examination_license_type']
             checklists = Checklist.objects.filter(examination__examination_date=str(examination_date), examination__examination_type=examination_type, examination__examination_license_type=examination_license_type)
+            
             if len(checklists) == 0:
                 errors = "No checklists available for the selection"
-                return JsonResponse({'success':False, 'errors': errors})
+                return HttpResponseNotFound(errors)
+        #        return JsonResponse({'success':False, 'errors': errors})
             else:
-                messages.success(request, "File found")
-                return JsonResponse(serializers.serialize('json', checklists), safe=False)
+                #messages.success(request, "File found")
+                checklist_files = []
+                for checklist in checklists:
+                    checklist_files.append('/media/'+str(checklist.checklist_file))
+
+                return HttpResponse(checklist_files)
         else:
             return JsonResponse({'success': False, 'errors': form.errors})
 
-            #return render(request, 'checklist/preview.html', {"checklists": checklists })
     else: 
         form = ExaminationForm()
         return render(request, 'checklist/index.html', {"form":form }) 
